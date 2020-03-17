5dc00c69e56eea6b9462a0d2fa7e3f5afaa1b167
fickrybiliman <bil.iman.fickry@gmail.com>
2020-03-17 19:06:23 +0700

    add testing package

---
+++ src/testings/TestModulus.java
@@ -0,0 +1,13 @@
+package testings;
+
+public class TestModulus {
+    public static void main(String[] args) {
+        int x = 17 % 3;
+        System.out.println(x);
+
+        double a = 20.45;
+        int b = 5;
+        int c = (int) a / b;
+        System.out.println(c);
+    }
+}
