42390e25624fbaf56d8eab28b6af9bfbd2ffd12b
fickrybiliman <bil.iman.fickry@gmail.com>
2020-03-26 03:03:00 +0700

    add testing code

--- src/testings/ParametersAndReturnType/Main.java
+++ src/testings/ParametersAndReturnType/Main.java
@@ -1,4 +1,51 @@
 package testings.ParametersAndReturnType;
 
 public class Main {
+    public static void main(String[] args) {
+
+        // please write the four methods as instructed below
+        // then call each method from here within the main()
+        int multiply = multiply(2, 8);
+        int divide = divide(8, 2);
+        boolean isOdd = isOdd(7);
+        String reverse = reverse("Programming is cool");
+
+        System.out.println(multiply);
+        System.out.println(divide);
+        System.out.println(isOdd);
+        System.out.println(reverse);
+    }
+
+    // please write a static multiply() method below that takes
+    // two parameters (argumets) and returns the result of
+    // multiplying them together
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // please write a static divide() method below that takes
+    // two parameters (argumets) and returns the result of
+    // dividing the first parameter by the second parameters
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    // please write a static isOdd() method that takes one numeric
+    // parameter and returns a true (boolean) if the parameter is odd
+    public static boolean isOdd(int num) {
+        return num % 2 != 0;
+    }
+
+    // please write a method that will take in a String (as an argument)
+    // and return that String in reverse order. For instance, if you pass
+    // in the String "programming is cool" the method will return the
+    // String "looc si gnimmargorp"
+    public static String reverse(String str) {
+        String result = "";
+        for (int i = str.length()-1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+
+        return result;
+    }
 }
--- src/testings/ReturnExample/Example.java
+++ src/testings/ReturnExample/Example.java
@@ -1,4 +1,15 @@
 package testings.ReturnExample;
 
 public class Example {
+    public static void main(String[] args) {
+        int x = multiply(2, 2);
+        int y = multiply(4, 4);
+
+        int z = multiply(x, y);
+        System.out.println(z);
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
 }
